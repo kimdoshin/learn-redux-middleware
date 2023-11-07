ff447ed5cb51b19c8a04c6917d95cf56581b9784
kimdoshin <kds6221@gmail.com>
2023-11-07 20:17:23 +0900

    비동기작어중

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,9 +1,10 @@
 import Counter from "../components/Counter";
 import {connect} from 'react-redux';
 import { increase, decrease } from "../modules/counter";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
 
-const CounterContainer = ({number, increase, decrease}) => {
-    return (<Counter number={number} onIncrease={increase} onDecrease={decrease} />);
+const CounterContainer = ({number, increaseAsync, decreaseAsync}) => {
+    return (<Counter number={number} onIncrease={increaseAsync} onDecrease={decreaseAsync} />);
 };
 
 
@@ -11,6 +12,6 @@ export default connect(
     state => ({
         number : state.counter,
     }), {
-        increase,
-        decrease
+        increaseAsync,
+        decreaseAsync
     })(CounterContainer);
